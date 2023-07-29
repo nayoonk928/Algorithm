2641d85a6ba89697a09b2738b5ceef5a08bbf623
Nayoon <Nayoon.Kim-1@student.uts.edu.au>
2023-07-29 21:55:28 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].contains(ex)) continue;
+            
+            answer.append(str_list[i]);
+        }
+        
+        return answer.toString();
+    }
+}
