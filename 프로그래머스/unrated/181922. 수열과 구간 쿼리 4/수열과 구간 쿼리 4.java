19328998fec0ef0d0f1f7f73609c44dfcdac51e0
Nayoon <nayoonk928@gmail.com>
2023-11-30 20:16:04 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 1.10 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = Arrays.copyOf(arr, arr.length);
+        
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for (int j = s; j <= e; j++) {
+                if (j % k == 0) {
+                    answer[j] += 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
