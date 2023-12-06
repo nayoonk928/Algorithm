94ce6655139574813df4a5c000ddc0b7d45123b8
Nayoon <nayoonk928@gmail.com>
2023-12-06 22:14:42 +0900

    Update 할인 행사.java

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,24 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int n = want.length;
+        // 원하는 상품명과 개수를 저장하는 맵
         Map<String, Integer> wantMap = new HashMap<>();
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
         
         for (int i = 0; i <= discount.length - 10; i++) {
+            // 10일치의 할인 품목 포함한 배열 생성
             String[] temp = Arrays.copyOfRange(discount, i, i + 10);
-
-            Map<String, Integer> tempMap = new HashMap<>();
-            for (String product : temp) {
-                tempMap.put(product, tempMap.getOrDefault(product, 0) + 1);
-            }
-
+            
             boolean isValid = true;
-            for (String key : wantMap.keySet()) {
-                if (!tempMap.containsKey(key) || tempMap.get(key) != wantMap.get(key)) {
+            for (int j = 0; j < want.length; j++) {
+                // 배열을 리스트로 변환하여 원하는 품목의 개수 확인
+                int count = Collections.frequency(Arrays.asList(temp), want[j]);
+                if (count != wantMap.get(want[j])) {
                     isValid = false;
                     break;
                 }
