ff6cdd696eb8314b791a2829d1d62b0352b49545
Nayoon <nayoonk928@gmail.com>
2023-12-25 18:44:18 +0900

    [Silver II] Title: 최대 힙, Time: 1068 ms, Memory: 32688 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (maxHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(maxHeap.poll());
+                }
+            } else {
+                maxHeap.add(num);
+            }
+        }
+    }
+}
