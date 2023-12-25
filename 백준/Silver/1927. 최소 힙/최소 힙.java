9a5e157c31e9f3d0369a983a2c3992faed7b13a0
Nayoon <nayoonk928@gmail.com>
2023-12-25 18:41:14 +0900

    [Silver II] Title: 최소 힙, Time: 1068 ms, Memory: 32936 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (minHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(minHeap.poll());
+                }
+            } else {
+                minHeap.add(num);
+            }
+        }
+    }
+}
