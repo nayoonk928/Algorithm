79511d3f5592f86d7151ad5b411f0d30d38943a0
Nayoon <nayoonk928@gmail.com>
2024-01-05 05:47:54 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 49784 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,12 @@
 n = int(input())
-a = set(map(int, input().split()))
+a = list(map(int, input().split()))
 m = int(input())
 targets = list(map(int, input().split()))
 
+a_set = set(a)
+
 for target in targets:
-    if target in a:
+    if target in a_set:
         print(1)
     else:
         print(0)
