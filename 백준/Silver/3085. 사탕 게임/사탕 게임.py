3792317315647448628278e7cdabc931dd7b1a30
Nayoon <nayoonk928@gmail.com>
2024-01-09 13:30:20 +0900

    [Silver II] Title: 사탕 게임, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,7 +1,3 @@
-# 백준 3085번 사탕 게임
-# 1. 사탕의 색이 다른 인접한 두 칸 고르기
-# 1-1. 1번에서 고른 사탕 서로 교환
-# 2. 모두 같은 색으로 이루어져 있는 가장 긴 연속 부분(행 or 열) 찾기
 import sys
 
 input = sys.stdin.readline
@@ -21,24 +17,24 @@ def max_candies(candies):
             if i + 1 < n:
                 right = candies[i + 1][j]
                 candies[i][j], candies[i + 1][j] = right, current
-                max_sum = max(max_sum, find_max_length(candies))
+                max_sum = max(max_sum, find_max_length(candies, i, i + 1, j, j))
                 candies[i][j], candies[i + 1][j] = current, right  # 원래대로 되돌리기
 
             # 아래쪽과 교환 (j + 1 < n인 경우)
             if j + 1 < n:
                 down = candies[i][j + 1]
                 candies[i][j], candies[i][j + 1] = down, current
-                max_sum = max(max_sum, find_max_length(candies))
+                max_sum = max(max_sum, find_max_length(candies, i, i, j, j + 1))
                 candies[i][j], candies[i][j + 1] = current, down  # 원래대로 되돌리기
 
     print(max_sum)
 
 
-def find_max_length(candies):
+def find_max_length(candies, row_start, row_end, col_start, col_end):
     max_length = 1
 
     # 행 기준으로 최대 연속 부분 찾기
-    for i in range(n):
+    for i in range(row_start, row_end + 1):
         length = 1
         for j in range(1, n):
             if candies[i][j] == candies[i][j - 1]:
@@ -46,12 +42,11 @@ def find_max_length(candies):
             else:
                 max_length = max(max_length, length)
                 length = 1
-                continue
 
         max_length = max(max_length, length)
 
     # 열 기준으로 최대 연속 부분 찾기
-    for j in range(n):
+    for j in range(col_start, col_end + 1):
         length = 1
         for i in range(1, n):
             if candies[i][j] == candies[i - 1][j]:
@@ -59,7 +54,6 @@ def find_max_length(candies):
             else:
                 max_length = max(max_length, length)
                 length = 1
-                continue
 
         max_length = max(max_length, length)
 
