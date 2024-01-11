f8e3e40ffcedd7e00a4c673d1e6df9d604abef68
Nayoon <nayoonk928@gmail.com>
2024-01-11 13:55:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -4,24 +4,14 @@ from collections import deque
 input = sys.stdin.readline
 
 
-def count_combinations(target):
-    ways = 0
+# dp
+def count_commbinations_dp(target):
+    dp = [1, 1, 2, 4]
 
-    queue = deque([0])
+    for i in range(4, target + 1):
+        dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
 
-    while queue:
-        current_sum = queue.popleft()
-
-        # 현재 합이 목표값에 도달한 경우
-        if current_sum == target:
-            ways += 1
-        # 현재 합이 목표값보다 작은 경우, 가능한 다음 값을 큐에 추가
-        elif current_sum < target:
-            queue.append(current_sum + 1)
-            queue.append(current_sum + 2)
-            queue.append(current_sum + 3)
-
-    return ways
+    return dp[target]
 
 
 if __name__ == "__main__":
@@ -29,4 +19,4 @@ if __name__ == "__main__":
 
     for _ in range(test_case):
         target = int(input())
-        print(count_combinations(target))
+        print(count_commbinations_dp(target))
