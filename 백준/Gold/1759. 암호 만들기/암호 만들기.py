e7d738b369a35116dafac0bff7f053b0f62b44d3
Nayoon <nayoonk928@gmail.com>
2024-01-11 10:24:18 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -3,31 +3,43 @@ from collections import deque
 
 input = sys.stdin.readline
 
-password_length, c = map(int, input().split())
-alphabet = list(input().split())
 
-vowels = set('aeiou')
+def generate_passwords(password_length, c, alphabet):
+    vowels = set('aeiou')
+    alphabet.sort()
+    queue = deque()
 
-alphabet.sort()
-queue = deque()
-
-# 알파벳을 초기 노드로 큐에 추가
-for char in alphabet:
-    queue.append(char)
-
-# bfs
-while queue:
-    current_node = queue.popleft()
-
-    if len(current_node) == password_length:
-        # 모음 개수 세기
-        vowel_count = sum(1 for char in current_node if char in vowels)
-        if 1 <= vowel_count <= password_length - 2:
-            print("".join(current_node))
-        continue
-
-    last_char = current_node[-1] if current_node else None
+    # 알파벳을 초기 노드로 큐에 추가
     for char in alphabet:
-        if not last_char or char > last_char:
-            new_node = current_node + char
-            queue.append(new_node)
+        queue.append(char)
+
+    result = []
+
+    # bfs
+    while queue:
+        current_node = queue.popleft()
+
+        if len(current_node) == password_length:
+            # 모음 개수 세기
+            vowel_count = sum(1 for char in current_node if char in vowels)
+            if 1 <= vowel_count <= password_length - 2:
+                result.append("".join(current_node))
+            continue
+
+        last_char = current_node[-1] if current_node else None
+        for char in alphabet:
+            if not last_char or char > last_char:
+                new_node = current_node + char
+                queue.append(new_node)
+
+    return result
+
+
+if __name__ == "__main__":
+    password_length, c = map(int, input().split())
+    alphabet = list(input().split())
+
+    passwords = generate_passwords(password_length, c, alphabet)
+
+    for password in passwords:
+        print(password)
