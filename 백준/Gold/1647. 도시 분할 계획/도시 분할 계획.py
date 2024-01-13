a34723d76a0314774657bcfb7abe7fcc44390070
Nayoon <nayoonk928@gmail.com>
2024-01-13 13:36:30 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 4188 ms, Memory: 371852 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,47 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def find(vertex):
+    if parent[vertex] != vertex:
+        parent[vertex] = find(parent[vertex])
+
+    return parent[vertex]
+
+
+def union(a, b):
+    a_parent = find(a)
+    b_parent = find(b)
+
+    parent[b_parent] = a_parent
+
+
+def get_minimum_cost(n, edges):
+    edges.sort(key=lambda x: x[2])
+    total_cost = 0
+    max_weight = 0
+
+    for idx, adj, weight in edges:
+        if find(idx) != find(adj):
+            union(idx, adj)
+            total_cost += weight
+            max_weight = max(max_weight, weight)
+
+    return total_cost - max_weight
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    edges = []
+    parent = [i for i in range(n + 1)]
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        if a != b:
+            graph[a].append((b, c))
+            graph[b].append((a, c))
+            edges.append((a, b, c))
+
+    print(get_minimum_cost(graph, edges))
