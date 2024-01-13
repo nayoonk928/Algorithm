b287b1ce4631e31ef8bbf92c8fdc5eed387ba7b2
Nayoon <nayoonk928@gmail.com>
2024-01-13 15:02:49 +0900

    [Gold IV] Title: 행성 연결, Time: 1276 ms, Memory: 177668 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -3,6 +3,7 @@ import sys
 input = sys.stdin.readline
 
 
+# 부모 찾기 & 업데이트
 def find(vertex):
     if parent[vertex] != vertex:
         parent[vertex] = find(parent[vertex])
@@ -14,16 +15,22 @@ def union(a, b):
     a_parent = find(a)
     b_parent = find(b)
 
-    parent[b_parent] = a_parent
+    if rank[a_parent] > rank[b_parent]:
+        parent[b_parent] = a_parent
+    else:
+        parent[a_parent] = b_parent
+
+        if rank[a_parent] == rank[b_parent]:
+            rank[b_parent] += 1
 
 
-def get_minimum_cost(n, costs):
-    costs.sort(key=lambda x: x[2])
+def get_minimum_cost(edges):
+    edges.sort(key=lambda x: x[2])
 
     min_sum = 0
     used_edges = 0
 
-    for idx, adj, cost in costs:
+    for idx, adj, cost in edges:
         if find(idx) != find(adj):
             union(idx, adj)
             min_sum += cost
@@ -37,13 +44,14 @@ def get_minimum_cost(n, costs):
 
 if __name__ == '__main__':
     n = int(input())
-    costs = []
+    edges = []
     parent = [i for i in range(n)]
+    rank = [0 for i in range(n)]
 
     for i in range(n):
         weights = list(map(int, input().split()))
         for j in range(n):
             if i != j:
-                costs.append((i, j, weights[j]))
+                edges.append((i, j, weights[j]))
 
-    print(get_minimum_cost(n, costs))
+    print(get_minimum_cost(edges))
