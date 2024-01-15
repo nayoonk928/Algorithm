e2e0600a97fdcc6f506c8a859f0edb0d842aba33
Nayoon <nayoonk928@gmail.com>
2024-01-15 16:29:25 +0900

    [Gold III] Title: 아기 상어, Time: 192 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,84 +1,62 @@
 import sys
 from collections import deque
+N = int(input())
 
-input = sys.stdin.readline
+dx = [-1,0,0,1] # 상 좌 우 하
+dy = [0,-1,1,0]
+room = []
+sharksize = 2
+sharkeat = 0
 
-# 상 좌 우 하
-dy = [-1, 0, 0, 1]
-dx = [0, -1, 1, 0]
+for i in range(N):
+    room.append([int(x) for x in sys.stdin.readline().rstrip().split()])
+    for j in range(len(room[i])):
+        if room[i][j] == 9:
+            room[i][j] = 0
+            shark_x, shark_y = i, j
 
+def finding_fish(sx,sy):
+    global sharksize
+    deq = deque()
+    deq.append([sx,sy])
 
-def bfs(start, target):
-    n = len(board)
-    visited = [[False] * n for _ in range(n)]
-    queue = deque([start])
-    visited[start[0]][start[1]] = True
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    distance = [[0 for _ in range(N)] for _ in range(N)]
+    can_eat_fish = []
 
-    while queue:
-        x, y, dist = queue.popleft()
-
-        if (x, y) == target:
-            return dist
+    while deq:
+        x, y = deq.popleft()
 
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and board[nx][ny] <= shark_size:
-                queue.append((nx, ny, dist + 1))
-                visited[nx][ny] = True
+            if 0 <= nx < N and 0 <= ny < N:
+                if room[nx][ny] <= sharksize and not visited[nx][ny]: #이동이 가능하면
+                    visited[nx][ny] = True
+                    distance[nx][ny] = distance[x][y] + 1
+                    deq.append([nx,ny])
 
-    return -1  # 목표에 도달할 수 없는 경우
+                    if room[nx][ny] < sharksize and room[nx][ny] != 0: #물고기가 있고 그걸 먹을 수 있다면
+                        can_eat_fish.append([nx ,ny,distance[nx][ny]])
 
-
-def find_fish(shark_x, shark_y):
-    n = len(board)
-    min_dist = float('inf')
-    target_fish = None
-
-    for i in range(n):
-        for j in range(n):
-            if 1 <= board[i][j] < shark_size and board[i][j] != 9:  # 먹을 수 있는 물고기인 경우
-                dist = bfs((shark_x, shark_y, 0), (i, j))
-                if dist != -1 and dist < min_dist:
-                    min_dist = dist
-                    target_fish = (i, j)
-
-    return target_fish, min_dist
-
-
-def find_shark():
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 9:
-                return (i, j)
-
+    can_eat_fish.sort(key= lambda x : (x[2],x[0],x[1])) # 정렬은 거리, x, y 오름차순으로
+    return can_eat_fish
 
 if __name__ == '__main__':
-    n = int(input())
-    board = []
-
-    for _ in range(n):
-        board.append(list(map(int, input().rstrip().split())))
-
-    shark_x, shark_y = find_shark()
-    shark_size = 2
-    eat_cnt = 0
-    time = 0
+    ans = 0
 
     while True:
-        fish, dist = find_fish(shark_x, shark_y)
-        if not fish:
-            break
+        fishlist = finding_fish(shark_x,shark_y)
 
-        x, y = fish
-        board[shark_x][shark_y] = 0  # 아기 상어 위치 초기화
-        shark_x, shark_y = x, y  # 아기 상어 위치 업데이트
-        board[x][y] = 9  # 아기 상어 이동
+        if len(fishlist) == 0: # 먹을 수 있는 물고기가 없다면
+            print(ans)
+            exit(0)
 
-        eat_cnt += 1
-        if eat_cnt == shark_size:  # 아기 상어 크기만큼 물고기를 먹으면 크기 증가
-            shark_size += 1
-            eat_cnt = 0
+        shark_x, shark_y, move_time = fishlist[0]
 
-        time += dist
+        sharkeat += 1
+        if sharksize == sharkeat: #먹은 물고기수와 사이즈가 같다면
+            sharkeat = 0
+            sharksize += 1
 
-    print(time)
+        room[shark_x][shark_y] = 0 # 물고기 먹은 자리는 빈칸으로 바꿈
+        ans += move_time
