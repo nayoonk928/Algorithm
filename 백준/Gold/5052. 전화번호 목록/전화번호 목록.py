1d40ad0795ab696c2e4811c0fd5bca92109177cc
Nayoon Kim <nayoonk928@gmail.com>
2024-01-17 14:53:46 +0900

    [Gold IV] Title: 전화번호 목록, Time: 164 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,54 +1,21 @@
 import sys
 
-input = sys.stdin.readline
+input = sys.stdin.readline    
 
 
-class Node:
-    def __init__(self):
-        self.data = {}
-        self.leaf = False
+t = int(input())
 
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-
-    def insert(self, num):
-        node = self.root
-        for n in num:
-            if n not in node.data:
-                node.data[n] = Node()
-            node = node.data[n]
-        node.leaf = True
-
-    def starts_with(self, prefix):
-        node = self.root
-        for n in prefix:
-            if n not in node.data:
-                return False
-            node = node.data[n]
-            if node.leaf:
-                return True
-        return False
-
-
-def is_valid(phone_nums):
-    trie = Trie()
-    phone_nums = sorted(phone_nums)
-
-    for num in phone_nums:
-        if trie.starts_with(num):
-            return "NO"
-        trie.insert(num)
-
-    return "YES"
-
-
-if __name__ == '__main__':
-    t = int(input())
-
-    for _ in range(t):
-        n = int(input())
-        phone_nums = [input().strip() for _ in range(n)]
-
-        print(is_valid(phone_nums))
+for _ in range(t):
+    n = int(input())
+    phone_nums = [input().strip() for _ in range(n)]
+    
+    phone_nums.sort()
+    is_valid = True
+    for i in range(n - 1):
+        if phone_nums[i] == phone_nums[i + 1][:len(phone_nums[i])]:
+            is_valid = False
+            print("NO")
+            break
+    
+    if is_valid:
+        print("YES")
