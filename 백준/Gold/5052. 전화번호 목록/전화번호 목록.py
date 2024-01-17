51361d0a5bb6173036b454dfd585d4e4cc6b65ed
Nayoon Kim <nayoonk928@gmail.com>
2024-01-17 14:23:33 +0900

    [Gold IV] Title: 전화번호 목록, Time: 948 ms, Memory: 46476 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,62 @@
+import sys
+
+input = sys.stdin.readline
+
+
+class Node:
+    def __init__(self):
+        self.data = {}
+        self.leaf = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, num):
+        node = self.root
+        for n in num:
+            if n not in node.data:
+                node.data[n] = Node()
+            node = node.data[n]
+        node.leaf = True
+
+    def search(self, num):
+        node = self.root
+        for n in num:
+            if n not in node.data:
+                return False
+            node = node.data[n]
+        return node.leaf
+
+    def starts_with(self, prefix):
+        node = self.root
+        for n in prefix:
+            if n not in node.data:
+                return False
+            node = node.data[n]
+            if node.leaf:
+                return True
+        return False
+
+
+def is_valid(phone_nums):
+    trie = Trie()
+    phone_nums = sorted(phone_nums)
+
+    for num in phone_nums:
+        if trie.search(num) or trie.starts_with(num):
+            return "NO"
+        trie.insert(num)
+
+    return "YES"
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        phone_nums = [input().strip() for _ in range(n)]
+
+        print(is_valid(phone_nums))
