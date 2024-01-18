29cc07e5660c5537357b0ccae536274098d2bda0
Nayoon Kim <nayoonk928@gmail.com>
2024-01-18 10:03:11 +0900

    [Silver III] Title: 주유소, Time: 140 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,23 +6,16 @@ distance = list(map(int, input().split()))
 oil = list(map(int, input().split()))
 
 result = 0
-curr_oil = 0
+curr_oil = float('inf')  
+min_oil_price = oil[0]  
+
 for idx, val in enumerate(distance):
-    if curr_oil >= val:
-        curr_oil -= val
+    min_oil_price = min(min_oil_price, oil[idx])  
+
+    if curr_oil > min_oil_price:  
+        result += min_oil_price * val
+        curr_oil = min_oil_price
     else:
-        oil_idx = 0  # 지금보다 싼 주유소 인덱스
-        for i in oil[idx:]:  # 지금보다 싼 주요소 위치 찾기
-            if i < oil[idx]:
-                oil_idx = oil.index(i)
-                break
-        if oil_idx == 0:  # 싼 곳이 없음
-            curr_oil += oil[idx] * sum(distance[idx:])
-            result += oil[idx] * sum(distance[idx:])
-            break
-        else:  # 싼 곳 있음, 그곳까지 갈 만큼만 구매
-            curr_oil += sum(distance[idx:oil_idx])
-            result += oil[idx] * sum(distance[idx:oil_idx])
-            curr_oil -= val
+        result += curr_oil * val
 
 print(result)
