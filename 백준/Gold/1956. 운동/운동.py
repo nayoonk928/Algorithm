23f9326f2c003dd3ce4531cc33453d4f7decaebf
Nayoon Kim <nayoonk928@gmail.com>
2024-01-20 16:44:55 +0900

    [Gold IV] Title: 운동, Time: 928 ms, Memory: 111920 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+V, E = map(int, input().split())
+
+graph = [[INF] * (V + 1) for _ in range(V + 1)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, V + 1):
+    for i in range(1, V + 1):
+        for j in range(1, V + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = INF
+for i in range(1, V + 1):
+    result = min(result, graph[i][i])
+
+if result == INF:
+    print(-1)
+else:
+    print(result)
