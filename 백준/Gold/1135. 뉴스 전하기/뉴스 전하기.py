262ef1f20e62fad3dec6893d67bb50a12dea58cb
Nayoon Kim <nayoonk928@gmail.com>
2024-04-19 09:18:23 +0900

    [Gold II] Title: 뉴스 전하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
@@ -1,28 +1,28 @@
 n = int(input())
-t = list(map(int, input().split()))
-tree = [[] for _ in range(n)]
+emp = list(map(int, input().split()))
+node = [[] for _ in range(n)]
+child_cnt = [0 for _ in range(n)]
 
-for idx in range(1, n):
-    tree[t[idx]].append(idx)  # 트리 생성
+def go(x):
+    global child_cnt
+    child_node = []
+    if not node[x]:
+        child_cnt[x] = 0
+    else:
+        for child in node[x]:
+            go(child)
+            child_node.append(child_cnt[child])
 
-# time[v] = v를 root로 하는 subtree에 정보를 모두 전달하는데 걸리는 시간
-time = [0] * n
+        child_node.sort(reverse=True)
+        child_node = [child_node[i] + i + 1 for i in range(len(child_node))]
+        child_cnt[x] = max(child_node)
 
-def dp(v):
-    child_t = []
-    for nei in tree[v]:
-        # Leaf까지 내려감
-        dp(nei)
-        # 각 child를 root로 하는 subtree에 정보 전달하는데 걸리는 시간 모음
-        child_t.append(time[nei])
-    if not tree[v]:
-        # Child가 없으면 0
-        child_t.append(0)
+# 직원의 상사-직속부하 관계 설정
+for i in range(1, len(emp)):
+    node[emp[i]].append(i)
 
-    child_t.sort(reverse=True)
-    # 시간이 오래 걸리는 쪽부터 먼저 전화를 돌리기
-    need_time = [child_t[i] + i + 1 for i in range(len(child_t))]
-    time[v] = max(need_time)  # 그 중에 가장 오래 걸리는 시간을 assign
+# 최대 깊이 구하기
+go(0)
 
-dp(0)
-print(time[0] - 1)  # Root node에 정보 전달하는 시간은 없으니 1빼기
+# 결과 출력
+print(child_cnt[0])
